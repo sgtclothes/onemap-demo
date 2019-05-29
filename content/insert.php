7b0ff35e8cb727f830890935610bc62744857b9b
Rizal Hermawan <rizalhrm24@gmail.com>
2019-05-29 09:24:01 +0700

    Edit Server Side Chrome PHP debugger (console.log)

--- content/insert.php
+++ content/insert.php
@@ -1,5 +1,5 @@
 <?php
-include 'config/ChromePhp.php';
+include '../config/ConsolePhp.php';
 $conn = new mysqli('localhost', 'root', '', 'demo_onemap');
 $type = $_POST['type'];
 $name = $_POST['name'];
@@ -10,9 +10,7 @@ $shape = $_POST['shape'];
 $created_by = $_POST['created_by'];
 $sql = "INSERT INTO `poi` (`id`, `type`, `name`, `lat`, `lon`, `region`, `shape`, `created_by`) VALUES ('', '$type', '$name', '$lat', '$lon', '$region', '$shape', '$created_by')";
 if ($conn->query($sql) === TRUE) {
-    ChromePhp::log('data inserted!');
+    ConsolePhp::log('data inserted!');
 } else {
-    ChromePhp::log('failed!');
+    ConsolePhp::log('failed!');
 }
-
-ChromePhp::log('HELLO WORLD');
