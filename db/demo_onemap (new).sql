12dfe880f06cecdd2675fd10510d4eac06589671
Rizal Hermawan <rizalhrm24@gmail.com>
2019-07-06 03:11:33 +0700

    update database

--- db/demo_onemap (new).sql
+++ db/demo_onemap (new).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 28, 2019 at 08:58 AM
+-- Generation Time: Jul 05, 2019 at 09:34 PM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 7.0.3
 
@@ -27,11 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `analysis` (
-  `id` int(11) NOT NULL,
+  `id` int(8) NOT NULL,
   `name` varchar(75) NOT NULL,
-  `created_by` int(11) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `created_by` int(4) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `analysis`
+--
+
+INSERT INTO `analysis` (`id`, `name`, `created_by`, `created_at`, `updated_at`) VALUES
+(1, 'test', 2, '2019-07-05 18:28:31', NULL),
+(2, 'test', 2, '2019-07-05 18:28:34', NULL),
+(3, 'test', 2, '2019-07-05 18:28:37', NULL);
 
 -- --------------------------------------------------------
 
@@ -40,11 +50,26 @@ CREATE TABLE `analysis` (
 --
 
 CREATE TABLE `analysis_points` (
-  `id` int(11) NOT NULL,
-  `analysis_id` int(11) DEFAULT NULL,
+  `id` int(8) NOT NULL,
+  `analysis_id` int(8) DEFAULT NULL,
   `lat` float NOT NULL,
-  `lon` float NOT NULL
+  `lon` float NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `analysis_points`
+--
+
+INSERT INTO `analysis_points` (`id`, `analysis_id`, `lat`, `lon`, `created_at`, `updated_at`) VALUES
+(1, 1, -8.654, 115.219, NULL, NULL),
+(2, 1, -8.487, 115.184, NULL, NULL),
+(3, 2, -8.654, 115.219, NULL, NULL),
+(4, 2, -8.501, 115.153, NULL, NULL),
+(5, 3, -8.654, 115.219, NULL, NULL),
+(6, 3, -8.472, 115.186, NULL, NULL),
+(7, 3, -8.817, 115.135, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -53,20 +78,22 @@ CREATE TABLE `analysis_points` (
 --
 
 CREATE TABLE `department` (
-  `id` int(11) NOT NULL,
-  `department` varchar(50) NOT NULL
+  `id` int(2) NOT NULL,
+  `department` varchar(50) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `department`
 --
 
-INSERT INTO `department` (`id`, `department`) VALUES
-(1, 'Locator Logic'),
-(2, 'Residential'),
-(3, 'Office'),
-(4, 'Industial'),
-(5, 'Investment and Advisory');
+INSERT INTO `department` (`id`, `department`, `created_at`, `updated_at`) VALUES
+(1, 'Locator Logic', NULL, NULL),
+(2, 'Residential', NULL, NULL),
+(3, 'Office', NULL, NULL),
+(4, 'Industial', NULL, NULL),
+(5, 'Investment and Advisory', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -75,51 +102,20 @@ INSERT INTO `department` (`id`, `department`) VALUES
 --
 
 CREATE TABLE `layer` (
-  `id` int(11) NOT NULL,
-  `name` varchar(40) NOT NULL
+  `id` int(3) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `layer`
 --
 
-INSERT INTO `layer` (`id`, `name`) VALUES
-(16, 'Bank DKI'),
-(27, 'Bank Mandiri'),
-(48, 'Bank Sumut');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `poi`
---
-
-CREATE TABLE `poi` (
-  `id` int(11) NOT NULL,
-  `type` varchar(35) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `lat` float NOT NULL,
-  `lon` float NOT NULL,
-  `region` varchar(50) NOT NULL,
-  `shape` varchar(50) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `color` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `poi`
---
-
-INSERT INTO `poi` (`id`, `type`, `name`, `lat`, `lon`, `region`, `shape`, `created_by`, `color`) VALUES
-(1, 'Minimarket', 'Indomaret', -5.753, 105.713, 'Where', '0.9292991929', 2, '#226AB3'),
-(2, 'Restaurant', 'DermagaSeafood', -6.4084, 107.097, 'cikarang', '6384723', 2, '#28B121'),
-(3, 'Office', 'Colliers Indonesia', -6.216, 106.82, 'Jakarta', '6384723', 2, '#E4FF00'),
-(4, 'Hospital', 'Omni Hospital', -7.156, 106.854, 'Sukabumi', '6384723', 2, '#226AB3'),
-(9, 'Office', 'Margo City Office', -6.392, 106.817, 'Depok', '6384723', 1, '#5B6128'),
-(16, 'XX', 'QQ', -1.329, 113.886, 'Depok', '6384723', 2, '#F30606'),
-(17, 'AA', 'BB', -1.264, 111.513, 'Kalimantan', '3', 2, '#F30606'),
-(18, 'CC', 'DD', 1.153, 109.975, 'Kalimantan', '3', 2, '#E110D5'),
-(19, 'EE', 'FF', 0.912, 110.897, 'Kalimantan', '3', 2, '#E110D5');
+INSERT INTO `layer` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(16, 'Bank DKI', NULL, NULL),
+(27, 'Bank Mandiri', NULL, NULL),
+(48, 'Bank Sumut', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -128,23 +124,25 @@ INSERT INTO `poi` (`id`, `type`, `name`, `lat`, `lon`, `region`, `shape`, `creat
 --
 
 CREATE TABLE `site` (
-  `id` int(11) NOT NULL,
+  `id` int(8) NOT NULL,
   `lat` float NOT NULL,
   `lon` float NOT NULL,
   `name` varchar(50) NOT NULL,
   `address` text NOT NULL,
-  `created_by` int(11) NOT NULL
+  `created_by` int(4) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `site`
 --
 
-INSERT INTO `site` (`id`, `lat`, `lon`, `name`, `address`, `created_by`) VALUES
-(1, -8.654, 115.219, 'Bar', 'Jalan Denpasar', 2),
-(5, -3.317, 105.047, 'site2', '', 2),
-(23, -6.883, 107.607, 'site 1', 'bandung', 2),
-(25, -2.598, 112.562, 'site night', '', 2);
+INSERT INTO `site` (`id`, `lat`, `lon`, `name`, `address`, `created_by`, `created_at`, `updated_at`) VALUES
+(1, -8.654, 115.219, 'Bar', 'Jalan Denpasar', 2, '2019-07-05 19:12:31', NULL),
+(5, -3.317, 105.047, 'site2', '', 2, '2019-07-05 19:12:35', NULL),
+(23, -6.883, 107.607, 'site 1', 'bandung', 2, '2019-07-05 19:12:39', NULL),
+(25, -2.598, 112.562, 'site night', '', 2, '2019-07-05 19:12:42', NULL);
 
 -- --------------------------------------------------------
 
@@ -153,12 +151,29 @@ INSERT INTO `site` (`id`, `lat`, `lon`, `name`, `address`, `created_by`) VALUES
 --
 
 CREATE TABLE `spec_buffer_analysis` (
-  `id` int(11) NOT NULL,
-  `analysis_points_id` int(11) DEFAULT NULL,
+  `id` int(8) NOT NULL,
+  `analysis_points_id` int(8) DEFAULT NULL,
   `distance` float NOT NULL,
   `unit` varchar(15) NOT NULL,
-  `options` int(1) NOT NULL DEFAULT '0'
+  `options` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `spec_buffer_analysis`
+--
+
+INSERT INTO `spec_buffer_analysis` (`id`, `analysis_points_id`, `distance`, `unit`, `options`, `created_at`, `updated_at`) VALUES
+(1, 1, 10, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(2, 1, 11, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(3, 2, 10, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(4, 2, 11, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(5, 3, 12, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(6, 4, 12, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(7, 5, 12, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(8, 6, 12, 'kilometers', 0, '2019-07-05 19:13:46', NULL),
+(9, 7, 12, 'kilometers', 0, '2019-07-05 19:13:46', NULL);
 
 -- --------------------------------------------------------
 
@@ -167,24 +182,26 @@ CREATE TABLE `spec_buffer_analysis` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(4) NOT NULL,
   `email` varchar(50) NOT NULL,
   `name` varchar(45) NOT NULL,
   `role` enum('System Administrator','Admin','User') NOT NULL,
   `password` varchar(255) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1'
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `name`, `role`, `password`, `active`) VALUES
-(1, 'system.administrator@locatorlogic.com', 'System Administrator', 'System Administrator', '1b3231655cebb7a1f783eddf27d254ca', 1),
-(2, 'rizal.hermawan@locatorlogic.com', 'Rizal Hermawan', 'Admin', '150fb021c56c33f82eef99253eb36ee1', 1),
-(3, 'sigit.sasongko@locatorlogic.com', 'Sigit Sasongko', 'Admin', '223a0fa8f15933d622b3c7a13f186027', 1),
-(4, 'adit@gmail.com', 'adit', 'User', '3bd862c0aa8fbe46cc60b2343e8d757f', 1),
-(5, 'rizalhrm24@gmail.com', 'Hermawan', 'User', '0a5c2657a27501b02b270ca999b0d412', 1);
+INSERT INTO `users` (`id`, `email`, `name`, `role`, `password`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'system.administrator@locatorlogic.com', 'System Administrator', 'System Administrator', '1b3231655cebb7a1f783eddf27d254ca', 1, NULL, NULL),
+(2, 'rizal.hermawan@locatorlogic.com', 'Rizal Hermawan', 'Admin', '150fb021c56c33f82eef99253eb36ee1', 1, NULL, NULL),
+(3, 'sigit.sasongko@locatorlogic.com', 'Sigit Sasongko', 'Admin', '223a0fa8f15933d622b3c7a13f186027', 1, NULL, NULL),
+(4, 'adit@gmail.com', 'adit', 'User', '3bd862c0aa8fbe46cc60b2343e8d757f', 1, NULL, NULL),
+(5, 'rizalhrm24@gmail.com', 'Hermawan', 'User', '0a5c2657a27501b02b270ca999b0d412', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -193,24 +210,26 @@ INSERT INTO `users` (`id`, `email`, `name`, `role`, `password`, `active`) VALUES
 --
 
 CREATE TABLE `users_department` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `department_id` int(11) NOT NULL
+  `id` int(5) NOT NULL,
+  `user_id` int(4) NOT NULL,
+  `department_id` int(2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users_department`
 --
 
-INSERT INTO `users_department` (`id`, `user_id`, `department_id`) VALUES
-(1, 1, 1),
-(2, 2, 1),
-(3, 3, 1),
-(4, 4, 2),
-(5, 4, 3),
-(7, 2, 4),
-(8, 5, 4),
-(9, 5, 5);
+INSERT INTO `users_department` (`id`, `user_id`, `department_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, NULL, NULL),
+(2, 2, 1, NULL, NULL),
+(3, 3, 1, NULL, NULL),
+(4, 4, 2, NULL, NULL),
+(5, 4, 3, NULL, NULL),
+(7, 2, 4, NULL, NULL),
+(8, 5, 4, NULL, NULL),
+(9, 5, 5, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -241,13 +260,6 @@ ALTER TABLE `department`
 --
 ALTER TABLE `layer`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `poi`
---
-ALTER TABLE `poi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user` (`created_by`);
 
 --
 -- Indexes for table `site`
@@ -285,42 +297,37 @@ ALTER TABLE `users_department`
 -- AUTO_INCREMENT for table `analysis`
 --
 ALTER TABLE `analysis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT for table `analysis_points`
 --
 ALTER TABLE `analysis_points`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `poi`
---
-ALTER TABLE `poi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `site`
 --
 ALTER TABLE `site`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 --
 -- AUTO_INCREMENT for table `spec_buffer_analysis`
 --
 ALTER TABLE `spec_buffer_analysis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 --
 -- AUTO_INCREMENT for table `users_department`
 --
 ALTER TABLE `users_department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -329,31 +336,25 @@ ALTER TABLE `users_department`
 -- Constraints for table `analysis`
 --
 ALTER TABLE `analysis`
-  ADD CONSTRAINT `fk_from_user_in_analysis` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `fk_from_user_in_analysis` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `analysis_points`
 --
 ALTER TABLE `analysis_points`
-  ADD CONSTRAINT `fk_from_analysis` FOREIGN KEY (`analysis_id`) REFERENCES `analysis` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
-
---
--- Constraints for table `poi`
---
-ALTER TABLE `poi`
-  ADD CONSTRAINT `fk_users` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_from_analysis` FOREIGN KEY (`analysis_id`) REFERENCES `analysis` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `site`
 --
 ALTER TABLE `site`
-  ADD CONSTRAINT `fk_from_user` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_users` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `spec_buffer_analysis`
 --
 ALTER TABLE `spec_buffer_analysis`
-  ADD CONSTRAINT `fk_from_analysis_points` FOREIGN KEY (`analysis_points_id`) REFERENCES `analysis_points` (`id`) ON DELETE SET NULL ON UPDATE SET NULL;
+  ADD CONSTRAINT `fk_from_analysis_points` FOREIGN KEY (`analysis_points_id`) REFERENCES `analysis_points` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users_department`
