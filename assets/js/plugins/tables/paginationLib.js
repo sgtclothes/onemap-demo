0b8eb6aa964dce6eb87b2366205e4ef68885e6c4
Rizal Hermawan <rizalhrm24@gmail.com>
2019-08-14 18:35:11 +0700

    create pagination library and dynamic detail analysis table

--- assets/js/plugins/tables/paginationLib.js
+++ assets/js/plugins/tables/paginationLib.js
@@ -1,96 +1,63 @@
-getPagination('.table-pagination-lib');
-//getPagination('.table-class');
-//getPagination('table');
-
-/*					PAGINATION 
-- on change max rows select options fade out all rows gt option value mx = 5
-- append pagination list as per numbers of rows / max rows option (20row/5= 4pages )
-- each pagination li on click -> fade out all tr gt max rows * li num and (5*pagenum 2 = 10 rows)
-- fade out all tr lt max rows * li num - max rows ((5*pagenum 2 = 10) - 5)
-- fade in all tr between (maxRows*PageNum) and (maxRows*pageNum)- MaxRows 
-*/
-
-function getPagination (table){
-    var lastPage = 1 ; 
-    $('#maxRows').on('change',function(evt){
-        //$('.pagination-libprev').html(''); // reset pagination 
-        lastPage = 1 ; 
-        $('.pagination-lib').find("li").slice(1, -1).remove();
-        var trnum = 0 ; // reset tr counter 
-        var maxRows = parseInt($(this).val()); // get Max Rows from select option
-        if(maxRows == 5 ){
-            $('.pagination-lib').hide();
-        } 
-        else {
-            $('.pagination-lib').show();
-        }
-        
-        var totalRows = $(table+' tbody tr').length;		// numbers of rows 
-        $(table+' tr:gt(0)').each(function(){			// each TR in  table and not the header
-            trnum++;									// Start Counter 
-            if (trnum > maxRows ){						// if tr number gt maxRows
-                $(this).hide();							// fade it out 
-            }
-            if (trnum <= maxRows ){
-                $(this).show();
-            }// else fade in Important in case if it ..
-        });				
-        							//  was fade out to fade it in 
-        if (totalRows > maxRows){						// if tr total rows gt max rows option
-            var pagenum = Math.ceil(totalRows/maxRows);	// ceil total(rows/maxrows) to get ..  
-                                         //	numbers of pages 
-            for (var i = 1; i <= pagenum ;){			// for each page append pagination li 
-                $('.pagination-lib #prev').before('<li data-page="'+i+'">\
-                  <span>'+ i++ +'<span class="sr-only">(current)</span></span>\
-                </li>').show();
-            }											
-        } 		
-
-        $('.pagination-lib [data-page="1"]').addClass('active'); // add active class to the first li 
-        $('.pagination-lib li').on('click',function(evt){		// on click each page
-            evt.stopImmediatePropagation();
-            evt.preventDefault();
-            var pageNum = $(this).attr('data-page');	// get it's number
-
-            var maxRows = parseInt($('#maxRows').val());			// get Max Rows from select option
-
-            if(pageNum == "prev" ){
-                if(lastPage == 1 ){return;}
-                pageNum  = --lastPage ; 
-            }
-            
-            if(pageNum == "next" ){
-                if(lastPage == ($('.pagination-lib li').length -2) ){return;}
-                pageNum  = ++lastPage ; 
-            }
-            
-            lastPage = pageNum ;
-            var trIndex = 0 ;							// reset tr counter
-            $('.pagination-lib li').removeClass('active');	// remove active class from all li 
-            $('.pagination-lib [data-page="'+lastPage+'"]').addClass('active');
-            
-            $(table+' tr:gt(0)').each(function(){		// each tr in table not the header
-                trIndex++;								// tr index counter 
-                if (trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum)-maxRows)){
-                    $(this).hide();		
+(function($) {
+    $(function() {
+        $.widget("zpd.paging", {
+            options: {
+                limit: 5,
+                rowDisplayStyle: 'block',
+                activePage: 0,
+                rows: []
+            },
+            _create: function() {
+                var rows = $("tbody", this.element).children();
+                this.options.rows = rows;
+                this.options.rowDisplayStyle = rows.css('display');
+                var nav = this._getNavBar();
+                this.element.after(nav);
+                this.showPage(0);
+            },
+            _getNavBar: function() {
+                var rows = this.options.rows;
+                var nav = $('<div>', {class: 'paging-nav'});
+                for (var i = 0; i < Math.ceil(rows.length / this.options.limit); i++) {
+                    this._on($('<a>', {
+                        href: '#',
+                        text: (i + 1),
+                        "data-page": (i)
+                    }).appendTo(nav),
+                            {click: "pageClickHandler"});
                 }
-                else {
-                    $(this).show();
-                } 				//else fade in 
-            }); 										// end of for each tr in table
-        });										// end of on click pagination list
-
-}).val(5).change();
-// end of on select change 
-// END OF PAGINATION 
-}	
-
-$(function(){
-// Just to append id number for each row  
-    $('table .table-pagination-lib tr:eq(0)').prepend('<th> ID </th>')
-    var id = 0;
-    $('table .table-pagination-lib tr:gt(0)').each(function(){	
-        id++
-        $(this).prepend('<td>'+id+'</td>');
+                return nav;
+            },
+            showPage: function(pageNum) {
+                var num = pageNum * 1; //it has to be numeric
+                this.options.activePage = num;
+                var rows = this.options.rows;
+                var limit = this.options.limit;
+                for (var i = 0; i < rows.length; i++) {
+                    if (i >= limit * num && i < limit * (num + 1)) {
+                        $(rows[i]).css('display', this.options.rowDisplayStyle);
+                    } else {
+                        $(rows[i]).css('display', 'none');
+                    }
+                }
+            },
+            pageClickHandler: function(event) {
+                event.preventDefault();
+                $(event.target).siblings().attr('class', "");
+                $(event.target).attr('class', "selected-page");
+                var pageNum = $(event.target).attr('data-page');
+                this.showPage(pageNum);
+            },
+            pageStepHandler: function(event) {
+                event.preventDefault();
+                //get the direction and ensure it's numeric
+                var dir = $(event.target).attr('data-direction') * 1;
+                var pageNum = this.options.activePage + dir;
+                //if we're in limit, trigger the requested pages link
+                if (pageNum >= 0 && pageNum < this.options.rows.length) {
+                    $("a[data-page=" + pageNum + "]", $(event.target).parent()).click();
+                }
+            }
+        });
     });
-})
+})(jQuery);
