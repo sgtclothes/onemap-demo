733112baff648913d93bc0212eda808c6b01c2e9
sgtclothes <siko.spade31@gmail.com>
2019-12-12 09:40:34 +0700

    adding basemaps from google

--- assets/js/filter/filter.js
+++ assets/js/filter/filter.js
@@ -289,23 +289,6 @@ var submitFilterServices = function (map) {
                 }, 10000);
                 checkHeightLegend()
             }
-
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                onOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-            })
-
-            Toast.fire({
-                icon: 'success',
-                title: 'Filter finished!'
-            })
         }
 
     });
